3c2244ad038d3f71646a0aee594bdc2e8f851964
jose654 <50951160+jose654@users.noreply.github.com>
2022-03-05 19:01:05 +0100

    Main menu and layout for game loop.

--- app/src/main/java/com/company/mathgame/MainActivity.java
+++ app/src/main/java/com/company/mathgame/MainActivity.java
@@ -2,13 +2,35 @@ package com.company.mathgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addition;
+    Button subtraction;
+    Button multi;
+    Button division;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addition = findViewById(R.id.buttonAddition);
+        subtraction = findViewById(R.id.buttonSubtraction);
+        multi = findViewById(R.id.buttonMultiplication);
+        division = findViewById(R.id.buttonDivision);
+
+        addition.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Game.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
